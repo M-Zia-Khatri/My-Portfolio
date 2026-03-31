312a65d24862576b32067914d03c5e9d13e5314a
M-Zia-Khatri <kb313892@gmail.com>
2026-03-31 14:12:40 +0500

    clean - remove the unvanted animtion variables for topbar

--- client/src/shared/components/layout/TopBar/TopBar.tsx
+++ client/src/shared/components/layout/TopBar/TopBar.tsx
@@ -2,7 +2,7 @@ import { AppNavigation } from '@/shared/constants/navigation.constants';
 import { TEXT } from '@/shared/constants/style.constants';
 import { cn } from '@/shared/utils/cn';
 import { Box, Button, Card, Container, Link, Text } from '@radix-ui/themes';
-import { motion, type Variants } from 'motion/react';
+import { motion } from 'motion/react';
 import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router';
 import { HIDE_DELAY_MS } from './TopBar.constants';
@@ -50,28 +50,6 @@ export default function TopBar() {
       if (hideTimer.current) clearTimeout(hideTimer.current);
     };
   }, []);
-  // ─────────────────────────────────────────────────────────────────────────
-
-  const itemVariants: Variants = {
-    initial: { y: 0, opacity: 0.9, fontWeight: 400 },
-    hover: {
-      y: -5,
-      opacity: 1,
-      fontWeight: 500,
-      scale: 1.02,
-      margin: '0 1%',
-      transition: { duration: 0.25, ease: 'easeOut' },
-    },
-  };
-
-  const underlineVariants: Variants = {
-    initial: { scaleX: 0, opacity: 0 },
-    hover: {
-      scaleX: 1,
-      opacity: 1,
-      transition: { delay: 0.1, duration: 0.3, ease: 'easeIn' },
-    },
-  };
 
   return (
     <Box asChild className="fixed top-4 z-50 w-full px-4">
